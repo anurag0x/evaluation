1887054ff2009c7c14486dbc8cdd09cc5ea1e596
unknown <anuragkushwah134@gmail.com>
2023-09-24 14:33:44 +0530

    done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors=require("cors")
 const app=express()
 app.use(express.json())
 app.use(cors())
-app.use(limiter)
+// app.use(limiter)
 app.use("/users",userrouter)
 app.use("/posts",postrouter)
 app.get("/",async(req,res)=>{
